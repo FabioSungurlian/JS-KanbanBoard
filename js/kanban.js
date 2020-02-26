701b42d1ad5780816303819003d21b5519da3f58
Fabio-Sungurlian <fsungurlian@gmail.com>
2020-02-26 12:45:39 -0300

    Set draggable in another way

--- js/kanban.js
+++ js/kanban.js
@@ -18,8 +18,8 @@ add_btn.addEventListener('click', () => {
 const create_item = () => {
   let item = document.createElement("div");
   item.classList.add("item");
-  item.id = `item-${order}`;
-  item.setAttribute("draggable", true);
+  item.id = "item-" + order;
+  item.draggable = true;
 
   item.addEventListener("dragstart", (event) => {
     return event.dataTransfer.setData("text", event.target.id);
@@ -54,12 +54,12 @@ const create_item = () => {
 };
 
 document.querySelectorAll('.drop').forEach(element => {
-  element.addEventListener("drop", () => {
+  element.addEventListener("drop", event => {
     event.preventDefault();
     const id = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(id));
   });
-  element.addEventListener("dragover", (event) => {
+  element.addEventListener("dragover", event => {
     event.preventDefault();
   });
 });
